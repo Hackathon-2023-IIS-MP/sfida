4f5cf7b48ae8b64635144b642f41b7a6d251ad2e
Filonzi Jacopo <st10357@iismarconipieralisi.it>
2023-12-22 20:47:18 +0100

    Multi-language setup

---
+++ auth/register/lang.php
@@ -0,0 +1,83 @@
+<?php
+
+$title = array(
+    'en' => 'Registration',
+    'it' => 'Registrazione'
+);
+
+
+$nameLabel = array(
+    'en' => 'Name',
+    'it' => 'Nome'
+);
+
+$surnameLabel = array(
+    'en' => 'Surname',
+    'it' => 'Cognome'
+);
+
+$birthayLabel = array(
+    'en' => 'Birthday',
+    'it' => 'Data di nascita'
+);
+
+$genreLabel = array(
+    'en' => 'Genre',
+    'it' => 'Genere'
+);
+
+$label_unselected = array(
+    'en' => 'Not selected',
+    'it' => 'Non selezionato'
+);
+
+$genreLabel_male = array(
+    'en' => 'Male',
+    'it' => 'Maschio'
+);
+
+$genreLabel_female = array(
+    'en' => 'Female',
+    'it' => 'Femmina'
+);
+
+$genreLabel_other = array(
+    'en' => 'Other',
+    'it' => 'Altro'
+);
+
+$cityLabel = array(
+    'en' => 'City',
+    'it' => 'Città'
+);
+
+$countryLabel = array(
+    'en' => 'Country',
+    'it' => 'Paese'
+);
+
+$passwordLabel = array(
+    'en' => 'Password',
+    'it' => 'Password'
+);
+
+$confirmPasswordLabel = array(
+    'en' => 'Confirm password',
+    'it' => 'Conferma password'
+);
+
+
+$registerBtn = array(
+    'en' => 'Register',
+    'it' => 'Registrati'
+);
+
+$otherwise1 = array(
+    'en' => 'Already own an account?',
+    'it' => 'Possiedi già un account?'
+);
+
+$otherwise2 = array(
+    'en' => 'Login',
+    'it' => 'Accedi'
+);
