0e4a8fad97f180b23a760f98bb81355aca8c7655
Driftini <yurisilveos741@gmail.com>
2023-12-23 09:10:52 +0100

    Logged-in variant of homepage, navbar indentation fix, removed unused CSS ID selector

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php 
 include $_SERVER['DOCUMENT_ROOT'] . '/inc/Top.php'; 
-
 ?>
 <!DOCTYPE html>
 <html lang="it">
@@ -15,36 +14,71 @@ include $_SERVER['DOCUMENT_ROOT'] . '/inc/Top.php';
     <?php include_once $_SERVER["DOCUMENT_ROOT"] . "/inc/Navbar.php" ?>
 
     <!-- Content div -->
-    <div id="home-jumbotron" class="p-5 text-center bg-body-secondary">
-        <div class="container py-5">
-            <h1 class="text-body-emphasis"><b>Connessione ed innovazione.</b></h1>
-            <p class="col-lg-8 mx-auto lead">
-                Uniamo progetti e idee in un'unica dimensione.
-            </p>
-            <div class="d-inline-flex gap-2 mb-5">
-                <a class="d-inline-flex align-items-center btn btn-primary btn-lg px-4 rounded-pill" href="/auth/register/">
-                    Registrati ora
-                </a>
-                <a class="btn btn-outline-secondary btn-lg px-4 rounded-pill" href="/auth/login/">
-                    Accedi
-                </a>
+    <?php
+    if (!isset($_COOKIE["email"]))
+    {
+        echo '
+        <div class="p-5 text-center bg-body-secondary shadow-lg">
+            <div class="container py-5">
+                <h1 class="text-body-emphasis"><b>Connessione ed innovazione.</b></h1>
+                <p class="col-lg-8 mx-auto lead">
+                    Uniamo progetti e idee in un\'unica dimensione.
+                </p>
+                <div class="d-inline-flex gap-2 mb-5">
+                    <a class="d-inline-flex align-items-center btn btn-primary btn-lg px-4 rounded-pill" href="/auth/register/">
+                        Registrati ora
+                    </a>
+                    <a class="btn btn-outline-secondary btn-lg px-4 rounded-pill" href="/auth/login/">
+                        Accedi
+                    </a>
+                </div>
             </div>
         </div>
-    </div>
+        ';
+    }
+    else
+    {
+        $u = clsUtenteBL::getUser($_COOKIE["email"]);
 
-    <div class="container my-5">
-        <?php
-            if (isset($_COOKIE["email"]))
-            {
-                $u = clsUtenteBL::getUser($_COOKIE["email"]);
-                $fullname = $u->getNome() . " " . $u->getCognome();
-                echo "Welcome $fullname! <a href='/auth/logout/'>Logout</a>";
-            }
-            else
-                echo "You are not logged in";
-        ?>
-        
-    </div>
+        echo '
+        <div class="container my-3 py-3">
+            <h1>Bentornato, <b>' . $u->getNome() . '</b>!</h1>
+            <h4 class="mb-5">Ecco alcuni progetti che potrebbero interessarti.</h4>
+
+            <div class="card mb-3 shadow-sm" style="max-width: 480px;">
+                <div class="row g-0">
+                    <div class="col-md-4" style="background: #0f7;">
+                        <img src="" class="img-fluid rounded-start">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Nome Progetto</h5>
+                            <p class="card-text text-body-secondary">
+                                Breve descrizione progetto.
+                                Breve descrizione progetto.
+                                Breve descrizione progetto.
+                                Breve descrizione progetto.
+                                Breve descrizione progetto.
+                            </p>
+                            <p class="card-text">
+                                <small class="text-body-tertiary">
+                                    <img style="width: 24px" class="rounded-pill border border-secondary d-inline-block mx-1 shadow-sm" src="/img/placeholder.png"> Luca Panieracci ·
+                                    Aggiornato 3 minuti fa
+                                </small>
+                            </p>
+                        </div>
+                        <div class="card-footer p-1 pt-0">
+                            <span class="badge bg-secondary">Biologia</span>
+                            <span class="badge bg-secondary">Informatica</span>
+                            <span class="badge bg-secondary">Economia</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        ';
+    }
+    ?>
 
     <!-- Include footer -->
     <?php include_once $_SERVER["DOCUMENT_ROOT"] . "/inc/Footer.php" ?>
